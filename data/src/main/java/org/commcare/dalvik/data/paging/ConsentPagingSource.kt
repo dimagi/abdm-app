e710fa61a9b1e5f496638645972ca60d964ff4fc
harish <h.rawatdelhi@gmail.com>
2024-01-10 00:43:31 +0530

    Error msg fix

--- data/src/main/java/org/commcare/dalvik/data/paging/ConsentPagingSource.kt
+++ data/src/main/java/org/commcare/dalvik/data/paging/ConsentPagingSource.kt
@@ -50,7 +50,7 @@ class ConsentPagingSource(
                 }
 
                 is HqResponseModel.AbdmError -> {
-                    LoadResult.Error(AbdmException(response.value.message))
+                    LoadResult.Error(AbdmException(response.value.getErrorMsg()))
                 }
 
                 else -> LoadResult.Error(RuntimeException("Error while loading..."))
